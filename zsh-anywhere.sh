edcf4713378a0d7ba11bb2199f7fa3d271dd3124
Zepeng <oliver_ck@outlook.com>
2023-04-26 11:03:41 +0800

    fix $CN control

--- zsh-anywhere.sh
+++ zsh-anywhere.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 NUM_STEP=4
-CN=${1:-1} # flag to use monitor address of China
+CN=${1:-1} # flag to use mirror address of China
 SOURCE_DIR=`pwd`
 
 printf "\033[1;32m Step[1/%d] \033[0m \033[5;33m Check and Install Zsh \033[0m \n" $NUM_STEP
 if ! command -v zsh &> /dev/null
 then
     echo "zsh not found. Installing zsh..."
-    if $CN
+    mkdir tmp
+    if [ $CN -eq 1 ]
     then
-    wget -c -O zsh.tar.xz -P ./tmp https://sourceforge.net/projects/zsh/files/latest/download
+    wget -c -O ./tmp/zsh.tar.xz https://sourceforge.net/projects/zsh/files/latest/download
     else
-    wget -c -O zsh.tar.xz -P ./tmp https://sourceforge.net/projects/zsh/files/latest/download
+    wget -c -O ./tmp/zsh.tar.xz https://sourceforge.net/projects/zsh/files/latest/download
     fi
     cd tmp
     tar -xf zsh.tar.xz
@@ -26,21 +27,21 @@ fi
 
 printf "\033[1;32m Step[2/%d] \033[0m \033[5;33m Install oh-my-zsh \033[0m \n" $NUM_STEP
 if [ ! -d "~/.oh-my-zsh" ]; then
-    if $CN
+    if [ $CN == '1' ]
     then
     git clone https://gitee.com/oliverck/ohmyzsh.git ~/.oh-my-zsh
     else
     git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
     fi
 else
-    echo "oh-my-zsh is already installed (remove ~/.oh-my-zsh if you want to re-deploy)"
+    echo "oh-my-zsh is already installed ( | rm -r ~/.oh-my-zsh | if you want to re-deploy)"
 fi
 
 printf "\033[1;32m Step[3/%d] \033[0m \033[5;33m Install plugins of ZSH \033[0m \n" $NUM_STEP
 cd ~/.oh-my-zsh/plugins
 printf "\033[1;32m Install plugin: zsh-autosuggestions \033[0m \n"
 if [ ! -d "./zsh-autosuggestions" ]; then
-    if $CN
+    if [ $CN == '1' ]
     then
     git clone https://gitee.com/oliverck/zsh-autosuggestions.git
     else
